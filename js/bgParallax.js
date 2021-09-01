62692c8ad9b3a40c6b797acf14c6fa59ea53a26b
sofihaure <89941385+sofihaure@users.noreply.github.com>
2021-09-01 16:59:59 -0300

    Add files via upload

---
+++ js/bgParallax.js
@@ -0,0 +1,27 @@
+(function(){
+//Objeto con propiedad de Parallax//
+var propParallax = {
+    seccion: document.querySelector('.parallax'),
+    recorrido: null,
+    limite: null
+}
+
+ //Objeto con método de Parallax//
+ var metParallax = {
+    inicio: function () {
+        window.addEventListener('scroll', metParallax.scrollParallax);
+    },
+    scrollParallax: function () {
+        propParallax.recorrido = window.pageYOffset;
+        propParallax.limite = propParallax.seccion.offsetTop + propParallax.seccion.offsetHeight;
+        if ( propParallax.recorrido > propParallax.seccion.offsetTop - window.outerHeight && propParallax.recorrido <= propParallax.limite ) {
+            propParallax.seccion.style.backgroundPositionY = (propParallax.recorrido - propParallax.seccion.offsetTop) / 1.5 + 'px';
+        } else{
+            propParallax.seccion.style.backgroundPositionY = 0;
+        }
+    }
+}
+metParallax.inicio();
+
+ }())
+ 
